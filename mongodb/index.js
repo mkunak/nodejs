5772fa70364401e07a2f137f91bd3b44ccce1346
Mykola Kunak <kunak.n@gmail.com>
2020-06-15 19:18:18 +0300

    node-mongodb: mondodb connection. start server function in index.js

--- mongodb/index.js
+++ mongodb/index.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const express = require('express');
 const exphbs = require('express-handlebars');
+const mongoose = require('mongoose');
 
 const homeRoutes = require('./src/routes/home');
 const cartRoutes = require('./src/routes/cart');
@@ -32,7 +33,26 @@ app.use('/cars', carsRoutes);
 app.use('/car-add', carAddRoutes);
 app.use('/cart', cartRoutes);
 
+const dbUser = 'mkunak';
+const dbName = 'nodejs-lessons';
+const password = 'Ovc3iVyYEdtHGKq4';
+const mongoUrl = `mongodb+srv://${dbUser}:${password}@cluster0-iixtt.mongodb.net/${dbName}?retryWrites=true&w=majority`;
 const PORT = process.env.PORT || 3001;
-app.listen(PORT, () => {
-  console.log(`>>> Server is running on port ${PORT}...`);
-});
+
+async function startServer() {
+  try {
+    await mongoose.connect(mongoUrl, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true
+    });
+
+    app.listen(PORT, () => {
+      console.log(`>>> Server is running on port ${PORT}...`);
+    });
+  } catch (error) {
+    console.log('>>> Start server: ', error);
+
+  }
+}
+
+startServer();
